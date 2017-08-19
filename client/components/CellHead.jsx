3a5c11e4f5734f0de158885208cf9f28a8a49808
interdigitize <interdigitize@users.noreply.github.com>
2017-08-18 17:34:33 -0700

    Update boostrap so it shows a grid on medium and small screens

--- client/components/CellHead.jsx
+++ client/components/CellHead.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var CellHead = (props) => {
 
 	return (
-		<div className="col-lg-1 bg-success" onClick={()=>{props.registerClick(props.rowIndex)}}>
+		<div className="col-xs-3 col-md-1 bg-success" onClick={()=>{props.registerClick(props.rowIndex)}}>
 			{props.beat || 'Undefined'}
 		</div>
 	)
