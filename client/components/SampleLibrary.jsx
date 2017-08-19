fd834f57d85a7b22ae029cf7f60377cf585f9dff
interdigitize <interdigitize@users.noreply.github.com>
2017-08-19 14:33:48 -0700

    delete unused SampleSound component and remove the import from the SampleLibrary

--- client/components/SampleLibrary.jsx
+++ client/components/SampleLibrary.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import SampleSound from './SampleSound.jsx';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
@@ -17,7 +15,7 @@ var SampleLibrary = (props) => {
           props.samples.map( (sound, index) => {
             console.log('SOUND', sound);
             return <div key={index} >
-              <Col xs={3} md={4} onClick={() => {props.sampleClick(index)}}>{sound.name}</Col>
+              <Col xs={3} md={4} onClick={() => {props.playSample(index)}}>{sound.name}</Col>
             </div>
           })
         }
