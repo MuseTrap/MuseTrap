45fa80d01a88d45ee96960893f6239f331f354d8
Matt Nicolaou <matthewpnicolaou@gmail.com>
2017-08-19 16:41:43 -0700

    removed updateUser

--- database/schema.js
+++ database/schema.js
@@ -59,22 +59,6 @@ let newUser = function(name, password) {
   });
 }
 
-let updateUser = function(sequence) {
-  var promise = Users.find().where('userName').equals(sequence.user).exec();
-
-  return promise.then(function(user) {
-    user.sequences.push(sequence);
-
-    return user.save(); 
-  })
-  .then(function(user) {
-    console.log('updated user: ' + user.name);
-  })
-  .catch(function(err){
-    console.log('error:', err);
-  });
-}
-
 let updateSequence = function(sequence) {
   var promise = Sequences.findById(sequence._id).exec();
 
