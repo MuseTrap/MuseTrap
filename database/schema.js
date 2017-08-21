94f4367509d027ac78e4d1fc3fd04d83a0d6525c
Steve Kuo <stevepkuo@gmail.com>
2017-08-21 01:37:55 -0700

    Change schema to point back to real database

--- database/schema.js
+++ database/schema.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 var Promise = require('bluebird');
 
-// var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-//                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };  
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };  
 
-// var mongodbUri = 'mongodb://heroku_tkrwhxb3:ljk1iikq8pag0rcusofkbir363@ds149613.mlab.com:49613/heroku_tkrwhxb3';
-// mongoose.connect(mongodbUri, options);
-mongoose.connect('mongodb://localhost/musetrap');
+var mongodbUri = 'mongodb://heroku_tkrwhxb3:ljk1iikq8pag0rcusofkbir363@ds149613.mlab.com:49613/heroku_tkrwhxb3';
+mongoose.connect(mongodbUri, options);
+
 var db = mongoose.connection;
 db.on('error', console.error);
 
