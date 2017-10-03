94f2301c609689fdcde3f42d645f83e64e96cccf
interdigitize <interdigitize@users.noreply.github.com>
2017-10-03 13:34:13 -0700

    Adjusts styles

--- client/components/CellHead.jsx
+++ client/components/CellHead.jsx
@@ -4,17 +4,14 @@ import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 var CellHead = (props) => {
 	var name;
 	var remove;
-	var inLineStyle = {'textAlign':'center'};
 	if (props.sample) {
 		name = props.sample.name;
 		remove = <Glyphicon glyph="remove-circle" onClick={ () => {props.unregisterSample(props.rowIndex)}}/>
 	}
 	return (
-		<div
-			className="col-xs-4 col-lg-4 bg-success" style={inLineStyle}
-		>
-			{props.sample ? name : 'Register Sound'}
-			{remove}
+		<div className="col-xs-3 col-md-offset-1 col-sm-2" style={{'textAlign':'right', 'color': 'white'}}>
+			{props.sample ? name : 'Add Sound'}
+			<span style={{'paddingLeft':'10px'}}>{remove}</span>
 		</div>
 	)
 
