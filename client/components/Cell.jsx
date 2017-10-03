a820562be1de1c1459613dcc4116128ec06d083c
interdigitize <interdigitize@users.noreply.github.com>
2017-10-03 13:33:09 -0700

    Adjusts responsive columns and adds glyph icons

--- client/components/Cell.jsx
+++ client/components/Cell.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
 var Cell = (props) => {
+  var icon;
+  if (props.cell === 0) {
+    icon = (<Glyphicon glyph="unchecked" />);
+  } else {
+    icon = (<Glyphicon glyph="stop" className={`color${props.rowIndex + 1}Font-pulse btnPulseOnce `}/>);
+  }
   return (
   	<div
   		style={{'textAlign':'center'}}
-      className="col-xs-4 col-md-1 bg-info" 
+      className={`col-xs-1` }
       onClick={ () => { props.toggleCell(props.rowIndex, props.colIndex) }}
     >
-      {props.cell}
+      {icon}
     </div>
   );
 };
